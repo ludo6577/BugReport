7aad8e54431d51d0bc1760aa1b053e87ab4d0c6f
ludo6577 <ludo6577@gmail.com>
2012-12-09 18:00:30 +0100

    CoptisBugReport first commit

---
+++ CoptisBugReport/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace CoptisBugReport
+{
+    partial class FormBugReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.labelTitre = new System.Windows.Forms.Label();
+            this.textBoxBugName = new System.Windows.Forms.TextBox();
+            this.richTextBoxDescription = new System.Windows.Forms.RichTextBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelPriority = new System.Windows.Forms.Label();
+            this.textBoxPriority = new System.Windows.Forms.TextBox();
+            this.labelGravity = new System.Windows.Forms.Label();
+            this.comboBoxGravity = new System.Windows.Forms.ComboBox();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Ref = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Titre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gravity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Priority = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonRecord.Location = new System.Drawing.Point(6, 304);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(189, 33);
+            this.buttonRecord.TabIndex = 6;
+            this.buttonRecord.Text = "Record";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(213, 304);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(189, 33);
+            this.buttonSend.TabIndex = 7;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // labelTitre
+            // 
+            this.labelTitre.AutoSize = true;
+            this.labelTitre.Location = new System.Drawing.Point(8, 14);
+            this.labelTitre.Name = "labelTitre";
+            this.labelTitre.Size = new System.Drawing.Size(31, 13);
+            this.labelTitre.TabIndex = 2;
+            this.labelTitre.Text = "Titre:";
+            // 
+            // textBoxBugName
+            // 
+            this.textBoxBugName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxBugName.Location = new System.Drawing.Point(52, 11);
+            this.textBoxBugName.Name = "textBoxBugName";
+            this.textBoxBugName.Size = new System.Drawing.Size(343, 20);
+            this.textBoxBugName.TabIndex = 2;
+            this.textBoxBugName.TextChanged += new System.EventHandler(this.Changed);
+            // 
+            // richTextBoxDescription
+            // 
+            this.richTextBoxDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxDescription.Location = new System.Drawing.Point(11, 112);
+            this.richTextBoxDescription.Name = "richTextBoxDescription";
+            this.richTextBoxDescription.Size = new System.Drawing.Size(388, 186);
+            this.richTextBoxDescription.TabIndex = 5;
+            this.richTextBoxDescription.Text = "";
+            this.richTextBoxDescription.TextChanged += new System.EventHandler(this.Changed);
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(11, 96);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(60, 13);
+            this.labelDescription.TabIndex = 5;
+            this.labelDescription.Text = "Description";
+            // 
+            // labelPriority
+            // 
+            this.labelPriority.AutoSize = true;
+            this.labelPriority.Location = new System.Drawing.Point(8, 40);
+            this.labelPriority.Name = "labelPriority";
+            this.labelPriority.Size = new System.Drawing.Size(41, 13);
+            this.labelPriority.TabIndex = 6;
+            this.labelPriority.Text = "Priority:";
+            // 
+            // textBoxPriority
+            // 
+            this.textBoxPriority.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPriority.Location = new System.Drawing.Point(52, 37);
+            this.textBoxPriority.Name = "textBoxPriority";
+            this.textBoxPriority.Size = new System.Drawing.Size(107, 20);
+            this.textBoxPriority.TabIndex = 3;
+            this.textBoxPriority.TextChanged += new System.EventHandler(this.TextChanged_NumericOnly);
+            // 
+            // labelGravity
+            // 
+            this.labelGravity.AutoSize = true;
+            this.labelGravity.Location = new System.Drawing.Point(8, 66);
+            this.labelGravity.Name = "labelGravity";
+            this.labelGravity.Size = new System.Drawing.Size(43, 13);
+            this.labelGravity.TabIndex = 8;
+            this.labelGravity.Text = "Gravity:";
+            // 
+            // comboBoxGravity
+            // 
+            this.comboBoxGravity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxGravity.FormattingEnabled = true;
+            this.comboBoxGravity.Items.AddRange(new object[] {
+            "1 - Critical",
+            "2 - Normal",
+            "3 - Minor"});
+            this.comboBoxGravity.Location = new System.Drawing.Point(52, 63);
+            this.comboBoxGravity.Name = "comboBoxGravity";
+            this.comboBoxGravity.Size = new System.Drawing.Size(107, 21);
+            this.comboBoxGravity.TabIndex = 4;
+            this.comboBoxGravity.TextChanged += new System.EventHandler(this.Changed);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(452, 384);
+            this.tabControl.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBoxDescription);
+            this.tabPage1.Controls.Add(this.comboBoxGravity);
+            this.tabPage1.Controls.Add(this.buttonRecord);
+            this.tabPage1.Controls.Add(this.labelGravity);
+            this.tabPage1.Controls.Add(this.buttonSend);
+            this.tabPage1.Controls.Add(this.textBoxPriority);
+            this.tabPage1.Controls.Add(this.labelTitre);
+            this.tabPage1.Controls.Add(this.labelPriority);
+            this.tabPage1.Controls.Add(this.textBoxBugName);
+            this.tabPage1.Controls.Add(this.labelDescription);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(405, 340);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Report";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(444, 358);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Existing bug";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ref,
+            this.Titre,
+            this.Description,
+            this.Gravity,
+            this.Priority});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(438, 352);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Ref
+            // 
+            this.Ref.HeaderText = "Ref";
+            this.Ref.Name = "Ref";
+            this.Ref.ReadOnly = true;
+            // 
+            // Titre
+            // 
+            this.Titre.HeaderText = "Titre";
+            this.Titre.Name = "Titre";
+            this.Titre.ReadOnly = true;
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            this.Description.ReadOnly = true;
+            // 
+            // Gravity
+            // 
+            this.Gravity.HeaderText = "Gravity";
+            this.Gravity.Name = "Gravity";
+            this.Gravity.ReadOnly = true;
+            // 
+            // Priority
+            // 
+            this.Priority.HeaderText = "Priority";
+            this.Priority.Name = "Priority";
+            this.Priority.ReadOnly = true;
+            // 
+            // FormBugReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 384);
+            this.Controls.Add(this.tabControl);
+            this.Name = "FormBugReport";
+            this.Text = "Bug reporter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormBugReport_FormClosing);
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label labelTitre;
+        private System.Windows.Forms.TextBox textBoxBugName;
+        private System.Windows.Forms.RichTextBox richTextBoxDescription;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelPriority;
+        private System.Windows.Forms.TextBox textBoxPriority;
+        private System.Windows.Forms.Label labelGravity;
+        private System.Windows.Forms.ComboBox comboBoxGravity;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ref;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gravity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Priority;
+    }
+}
+
---
+++ CoptisBugReport/Form1.cs
@@ -0,0 +1,246 @@
+﻿using AviFile;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace CoptisBugReport
+{
+    public partial class FormBugReport : Form
+    {
+        #region Variables
+
+        private static String VIDEO_FILENAME = @"../videoTest.avi";
+
+        private AviManager aviManager;
+        private VideoStream aviStream;
+        private Bitmap bitmap;
+
+        private Thread threadRecorder;
+        private bool threadRecording;
+        private bool recorded;
+
+        String currentScreen;
+
+        #endregion
+
+        #region Initialisation
+
+        public FormBugReport()
+        {
+            InitializeComponent();
+            ScreenChanged("Coptis");
+        }
+
+        #endregion
+
+        #region TODO
+
+        /*
+         *  TODO : Appeller cette méthode depuis Coptis et charger les données depuis l'intranet!
+         */ 
+        public void ScreenChanged(String screenName)
+        {
+            /*
+             *  TODO : Charger les données depuis l'intranet !
+             *  (bugs correspondant a l'écran)
+             */ 
+            dataGridView.Rows.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                dataGridView.Rows.Add(new Object[] { "Ref", "Titre", "Description", "Gravity", "Priority"});
+            }
+        }
+
+
+        /*  
+         *  TODO: Envoyer les information a l'intranet (créer un bug)
+         */ 
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            /*  TODO: send to intranet
+             *  _The video:         ../videoTest.avi
+             *  _Titre:             textBoxTitre.Text
+             *  _Description:       richTextBoxDescription.Text
+             *  _Priority:          textBoxPriority.Text
+             *  _Gravity:           comboBoxGravity.Text
+             *  _Screen:            (la je sais pas)
+             */
+            String title = textBoxBugName.Text;
+            String description = richTextBoxDescription.Text;
+            String priority = textBoxPriority.Text;
+            String gravity = comboBoxGravity.Text;
+            //FileStream video = File.OpenRead(VIDEO_FILENAME);
+            //String Screen = Variables.Screen;
+            //... (autre variables utiles)
+
+            //TODO : SEND THESE INFORMATIONS TO THE INTRANET !!!!!!!!!!!!
+
+            //A voir on pourrais ouvrir la page correspondant au bug directement dans le navigateur par défaut
+            this.Close();
+        }
+
+        #endregion
+
+        # region Recording methods
+
+        /*
+         *  Initialise the recording attributes
+         *  (Automatic call to the record method)
+         */ 
+        private bool startRecording(String fileName)
+        {
+            try
+            {
+                //Set the first image of the video
+                int ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
+                int ScreenHeight = Screen.PrimaryScreen.Bounds.Height;
+                bitmap = new Bitmap(ScreenWidth, ScreenHeight);
+                Graphics.FromImage(bitmap).DrawImage(Image.FromFile(@"../Coptis.png"), 0, 0, ScreenWidth, ScreenHeight);
+
+                aviManager = new AviManager(fileName, false);
+                aviStream = aviManager.AddVideoStream(false, 4, bitmap);
+                aviStream.GetFrameOpen();
+
+                //The Recording Thread
+                threadRecorder = new Thread(new ThreadStart(Record));
+                threadRecording = true;
+                recorded = true;
+                threadRecorder.Start();
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error occured. Please reload the program:" + Environment.NewLine + e.Message, "Coptis bug reporter");
+                return false;
+            }
+        }
+
+        /*
+         *  Recording method
+         *  Must be called in a Thread
+         */ 
+        private void Record()
+        {
+            while (threadRecording)
+            {
+                try{
+                    Graphics.FromImage(bitmap).CopyFromScreen(Point.Empty, Point.Empty, Screen.PrimaryScreen.Bounds.Size);
+                    aviStream.AddFrame(bitmap);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error occured. Please reload the program:" + Environment.NewLine + e.Message, "Coptis bug reporter");
+                }
+            }
+        }
+
+        /*
+         *  Stop the recording
+         *  Bug if you try to record again
+         */ 
+        private void stopRecording()
+        {
+            try{
+                threadRecording = false;
+                aviStream.GetFrameClose();
+               // aviStream.Close();
+                aviManager.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error occured. Please reload the program:" + Environment.NewLine + e.Message, "Coptis bug reporter");
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /*
+         *  Check if we are allowed to send the report
+         *  Check if all the textBox/comboBox are filled and we aren't recording
+         */ 
+        private bool CheckAllowedToSend()
+        {
+            if (textBoxBugName.Text != "" && richTextBoxDescription.Text != "" && textBoxPriority.Text != "" && comboBoxGravity.Text != "" && threadRecording==false && recorded==true)
+            {
+                buttonSend.Enabled = true;
+                return true;
+            }
+            else
+            {
+                buttonSend.Enabled = false;
+                return false;
+            }
+        }
+
+        /*
+         *  Click event on the record button
+         */ 
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+            if(buttonRecord.Text=="Record")
+            {
+                if (startRecording(VIDEO_FILENAME) == true)
+                    buttonRecord.Text = "Stop recording";
+                else
+                    buttonRecord.Text = "Record";
+            }
+            else if (buttonRecord.Text == "Stop recording")
+            {
+                buttonRecord.Text = "Record";
+                stopRecording();
+            }
+
+            CheckAllowedToSend();
+        }
+
+        /*
+         *  When something change (text in textBox or index in comboBox)
+         *  Test if we can send the report
+         */ 
+        private void Changed(object sender, EventArgs e)
+        {
+            CheckAllowedToSend();
+        }
+
+        private void TextChanged_NumericOnly(object sender, EventArgs e)
+        {
+            TextBox textBox = ((TextBox)sender);
+
+            for (int i = 0; i < textBox.Text.Length; i++)
+            {
+                char lastChar = textBox.Text[i];
+                if (lastChar < '0' || lastChar > '9')
+                {
+                    textBox.Text = textBox.Text.Remove(i, 1);
+                    textBox.SelectionStart = textBox.Text.Length;
+                }
+            }
+
+            Changed(sender, e);
+        }
+
+        private void FormBugReport_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (threadRecording == true)
+            {
+                e.Cancel = true;
+                MessageBox.Show("Stop recording first", "Coptis bug reporter");
+            }
+            else
+            {
+                File.Delete(VIDEO_FILENAME);
+            }
+        }
+
+        #endregion
+    }
+}
